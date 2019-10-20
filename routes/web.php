905ae2ae3360da8e33ee02756436ffcac036abd6
rizkyfernanda <mrizkyfer@gmail.com>
2019-10-20 20:13:03 +0700

    hapus yg gak perlu

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,3 @@ Route::get('/', function () {
 Route::get('dosen','ControllerDosen@index');
 
 
-
-//CRUD Pegawai -------------------------------------------------------
-Route::get('pegawai','ControllerPegawai@index');
-
-//View Pegawai
-Route::get('pegawai/view/id={id}','ControllerPegawai@view');
-
-//Tambah pegawai
-Route::get('pegawai/formulir','ControllerPegawai@formulir');
-Route::post('pegawai/formulir/proses', 'ControllerPegawai@proses');
-
-//Edit pegawai
-Route::get('pegawai/edit/id={id}', 'ControllerPegawai@edit');
-Route::post('pegawai/update', 'ControllerPegawai@update');
-
-//Hapus pegawai
-Route::get('pegawai/hapus/id={id}', 'ControllerPegawai@hapus');
