700de8b8e6e38a0c4bda2ad1f4ec99c07103af9e
azharabd <azhar.razar@gmail.com>
2019-11-27 15:32:33 +0700

    added custommer route

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,18 @@ Route::get('/', function () {
 //Buat Customer
 Route::get('/check-identity', 'CustomerController@checkIdentity'); //Check Identity of maid
 Route::get('/check-identity/search/', 'CustomerController@identitySearch'); //Search Identity of maid
+Route::get('/view-maids/search', 'CustomerController@search_maids'); //Search form 
+Route::get('/view-maids/result', 'CustomerController@result_maids'); //Result from search maid
+Route::get('/view-maids/{maid_id}', 'CustomerController@maid_details'); //Maid details
+Route::post('/view-maids/save-maid', 'CustomerController@save_maid'); //Add saved maid
+Route::post('/view-maids/order-maid', 'CustomerController@order_maid'); //Add ordered maid
 
 //Buat Agent
 Route::get('/agent', 'AgentController@index')->middleware('is_agent');
 Route::get('/agent/view-users', 'AgentController@view_users')->middleware('is_agent');
 Route::get('/agent/view-maids', 'AgentController@view_maids')->middleware('is_agent');
 Route::get('/agent/view-maids/add', 'AgentController@add')->middleware('is_agent');
+ROute::get('/agent/view-maids/{maid_id}', 'AgentController@maid_details')->middleware('is_agent');
 Route::post('/agent/view-maids/add/store', 'AgentController@store')->middleware('is_agent');
 Route::get('/agent/view-maids/edit/{maid_id}', 'AgentController@edit')->middleware('is_agent');
 Route::post('/agent/view-maids/edit/update', 'AgentController@update')->middleware('is_agent');
